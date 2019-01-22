6190dbfeaefbfa1e1f0136550f9316e6df9d8466
Navin Dhuttargi <46275302+navinexioms@users.noreply.github.com>
2019-01-22 19:16:15 +0530

    Did the linking part
    Did the linking part

---
+++ Ludo/Assets/ExtraSceneController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ExtraSceneController : MonoBehaviour 
+{
+	public void LoadExtraScene()
+	{
+		SceneManager.LoadScene ("ExtraScenes");
+	}
+	public void LoadProfileScene()
+	{
+		SceneManager.LoadScene ("ProfileScene");
+	}
+	public void WalletScene()
+	{
+		SceneManager.LoadScene ("WalletScene");
+	}
+	public void  LoadTransactionScene()
+	{
+		SceneManager.LoadScene ("TransactioScene");
+	}
+	public void SettingScene()
+	{
+		SceneManager.LoadScene ("SettingScene");
+	}
+	public void Logout()
+	{
+		
+	}
+	// Use this for initialization
+
+}
