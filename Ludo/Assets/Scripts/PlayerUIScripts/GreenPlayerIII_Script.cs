a547bf6ede2e1a15e4aca6c2e04a555bd60689f3
Navin Dhuttargi <46275302+navinexioms@users.noreply.github.com>
2019-01-24 11:50:30 +0530

    Added New Components to the Players 4 Elements
    Added New Components to the Players 4 Elements like box collider and rigidbody

---
+++ Ludo/Assets/Scripts/PlayerUIScripts/GreenPlayerIII_Script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenPlayerIII_Script : MonoBehaviour {
+
+	// Use this for initialization
+	public static string GreenPlayerIII_ColName;
+
+	void OnTriggerEnter(Collider col)
+	{
+
+		if (col.gameObject.tag == "blocks") 
+		{
+
+			GreenPlayerIII_ColName = col.gameObject.name;
+
+			if (col.gameObject.name.Contains ("Safe House")) 
+			{
+
+				print ("Entered PlayerI GreenI in safe house");
+
+			}
+		}
+	}
+	// Use this for initialization
+	void Start () 
+	{
+
+		GreenPlayerIII_ColName = "none";
+
+	}
+}
